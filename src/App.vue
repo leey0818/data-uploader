cf9294d6b12cf03e8b5d9b736d2ab76f191bab74
leey0818 <leey0818@gmail.com>
2020-01-05 00:57:38 +0900

    readFile 함수 추가 및 시트 선택 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,33 @@
 
       <file-select
         v-model="file"
-        :loading="file ? '파일 가져오는중...' : null"
+        :loading="loading"
+        loadingText="파일 읽어오는중..."
       ></file-select>
+
+      <b-form-group
+        label-cols="3"
+        label-cols-sm="2"
+        label-size="sm"
+        label="시트 선택"
+        v-if="sheetNames.length > 1"
+      >
+        <b-form-select
+          class="col-sm-6 col-lg-5"
+          size="sm"
+          v-model="currentSheetName"
+          :options="sheetNames"
+        ></b-form-select>
+      </b-form-group>
     </b-container>
   </div>
 </template>
 
 <script>
+import XLSX from 'xlsx';
 import FileSelect from './components/FileSelect.vue';
+
+let workbook;
 
 export default {
   name: 'app',
@@ -22,7 +41,51 @@ export default {
   data() {
     return {
       file: null,
+      loading: false,
+      currentSheetName: null,
+      sheetNames: [],
     };
+  },
+  watch: {
+    file(v) {
+      if (v === null) {
+        this.workbook = null;
+        this.currentSheetName = null;
+        this.sheetNames = [];
+      } else {
+        this.readFile(v);
+      }
+    },
+  },
+  methods: {
+    readFile(file) {
+      // read start
+      this.loading = true;
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = new Uint8Array(e.target.result);
+
+        // read file
+        workbook = XLSX.read(data, { type: 'array' });
+        this.loading = false;
+        this.sheetNames = workbook.SheetNames;
+        this.currentSheetName = this.sheetNames[0] || null;
+
+        const firstSheet = workbook.Sheets[this.currentSheetName];
+        const sheetData = XLSX.utils.sheet_to_json(firstSheet, { header: 1 });
+        const maxCol = sheetData.map(v => v.length).reduce((p, c) => Math.max(p, c));
+
+        console.log(workbook);
+        console.log(maxCol);
+        console.log(sheetData);
+      };
+      reader.onprogress = (e) => {
+        this.percent = (e.total / e.loaded).toFixed(2);
+      };
+
+      reader.readAsArrayBuffer(file);
+    },
   },
 };
 </script>

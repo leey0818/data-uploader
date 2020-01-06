627100a63cc902b75811aacbae2913e1465e1e17
leey0818 <leey0818@gmail.com>
2020-01-06 23:41:05 +0900

    시트 데이터 표시 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,26 @@
           :options="sheetNames"
         ></b-form-select>
       </b-form-group>
+
+      <b-table
+        :fields="fields"
+        :items="items"
+        bordered
+        fixed
+        responsive
+        small
+      >
+        <template v-slot:table-colgroup="scope">
+          <col
+            v-for="field in scope.fields"
+            :key="field.key"
+            style="width:140px"
+          >
+        </template>
+        <template v-slot:cell()="data">
+          <span :title="data.value">{{ data.value }}</span>
+        </template>
+      </b-table>
     </b-container>
   </div>
 </template>
@@ -44,6 +64,8 @@ export default {
       loading: false,
       currentSheetName: null,
       sheetNames: [],
+      fields: [],
+      items: [],
     };
   },
   watch: {
@@ -54,6 +76,27 @@ export default {
         this.sheetNames = [];
       } else {
         this.readFile(v);
+      }
+    },
+
+    currentSheetName(v) {
+      if (v === null) {
+        this.fields = [];
+        this.items = [];
+      } else {
+        const sheet = this.getSheet(v);
+        if (sheet) {
+          const maxCol = sheet.map(o => o.length).reduce((p, c) => Math.max(p, c));
+          this.fields = Array(maxCol).fill().map((_, i) => ({
+            key: `row_${i}`,
+            label: `열 ${i + 1}`,
+            tdClass: 'text-ellipsis',
+          }));
+          this.items = sheet.map(r => r.reduce((acc, cur, i) => {
+            acc[`row_${i}`] = cur;
+            return acc;
+          }, {}));
+        }
       }
     },
   },
@@ -86,6 +129,13 @@ export default {
 
       reader.readAsArrayBuffer(file);
     },
+
+    getSheet(name) {
+      if (workbook && workbook.Sheets[name]) {
+        return XLSX.utils.sheet_to_json(workbook.Sheets[name], { header: 1 });
+      }
+      return null;
+    },
   },
 };
 </script>
@@ -100,5 +150,10 @@ export default {
 }
 .border-thin {
   border-width: thin !important;
+}
+.text-ellipsis {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
 }
 </style>

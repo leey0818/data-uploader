8259042c638962335e99fa1bd63210aa4bb63756
leey0818 <leey0818@gmail.com>
2020-01-13 22:43:20 +0900

    UI Framework 변경
    boostrap-vue 에서 vuetify 로 변경

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,43 @@
 <template>
   <div id="app">
-    <b-container fluid>
+    <v-app>
+      <v-content>
+        <v-container fluid>
+
+          <v-stepper v-model="step" vertical>
+            <v-stepper-step :complete="step > 1" step="1">파일 선택</v-stepper-step>
+            <v-stepper-content step="1">
+              <file-select v-model="file" placeholder="파일을 선택하세요." :loading="loading"></file-select>
+            </v-stepper-content>
+
+            <v-stepper-step :complete="step > 2" step="2">
+              시트 선택 <small v-if="step > 2">"{{ currentSheetName }}" 선택됨</small>
+            </v-stepper-step>
+            <v-stepper-content step="2">
+              <v-select v-model="currentSheetName" dense :items="sheetNames"></v-select>
+              <v-btn
+                color="primary"
+                depressed
+                small
+                :disabled="currentSheetName === null"
+                @click="step = 3"
+              >선택완료</v-btn>
+              <v-btn class="ml-1" depressed small @click="file = null">이전으로</v-btn>
+            </v-stepper-content>
+
+            <v-stepper-step step="3">매핑할 데이터 선택</v-stepper-step>
+            <v-stepper-content step="3">
+              <!-- todo -->
+            </v-stepper-content>
+          </v-stepper>
+        </v-container>
+      </v-content>
+    </v-app>
+    <!-- <b-container fluid>
       <h4 class="mb-4">Grid File Uploader</h4>
 
-      <file-select
-        v-model="file"
-        :loading="loading"
-        loadingText="파일 읽어오는중..."
-      ></file-select>
-
-      <b-form-group
-        label-cols="3"
-        label-cols-sm="2"
-        label-size="sm"
-        label="시트 선택"
-        v-if="sheetNames.length > 1"
-      >
-        <b-form-select
-          class="col-sm-6 col-lg-5"
-          size="sm"
-          v-model="currentSheetName"
-          :options="sheetNames"
-        ></b-form-select>
-      </b-form-group>
-
-      <b-table
-        :fields="fields"
-        :items="items"
-        bordered
-        fixed
-        responsive
-        small
-      >
-        <template v-slot:table-colgroup="scope">
-          <col
-            v-for="field in scope.fields"
-            :key="field.key"
-            style="width:140px"
-          >
-        </template>
-        <template v-slot:cell()="data">
-          <span :title="data.value">{{ data.value }}</span>
-        </template>
-      </b-table>
-    </b-container>
+      <data-table :headers="headers" :items="items"></data-table>
+    </b-container> -->
   </div>
 </template>
 
@@ -60,42 +54,38 @@ export default {
   },
   data() {
     return {
+      step: 1,
       file: null,
       loading: false,
       currentSheetName: null,
-      sheetNames: [],
-      fields: [],
+      sheetNames: [{ value: null, text: '데이터를 가져올 시트를 선택하세요' }],
+      headers: [],
       items: [],
     };
   },
   watch: {
     file(v) {
       if (v === null) {
+        this.step = 1;
         this.workbook = null;
         this.currentSheetName = null;
-        this.sheetNames = [];
+        this.sheetNames.splice(1, this.sheetNames.length - 1);
       } else {
+        this.step = 2;
         this.readFile(v);
       }
     },
 
     currentSheetName(v) {
       if (v === null) {
-        this.fields = [];
+        this.headers = [];
         this.items = [];
       } else {
         const sheet = this.getSheet(v);
         if (sheet) {
           const maxCol = sheet.map(o => o.length).reduce((p, c) => Math.max(p, c));
-          this.fields = Array(maxCol).fill().map((_, i) => ({
-            key: `row_${i}`,
-            label: `열 ${i + 1}`,
-            tdClass: 'text-ellipsis',
-          }));
-          this.items = sheet.map(r => r.reduce((acc, cur, i) => {
-            acc[`row_${i}`] = cur;
-            return acc;
-          }, {}));
+          this.headers = Array(maxCol).fill({});
+          this.items = sheet;
         }
       }
     },
@@ -112,19 +102,7 @@ export default {
         // read file
         workbook = XLSX.read(data, { type: 'array' });
         this.loading = false;
-        this.sheetNames = workbook.SheetNames;
-        this.currentSheetName = this.sheetNames[0] || null;
-
-        const firstSheet = workbook.Sheets[this.currentSheetName];
-        const sheetData = XLSX.utils.sheet_to_json(firstSheet, { header: 1 });
-        const maxCol = sheetData.map(v => v.length).reduce((p, c) => Math.max(p, c));
-
-        console.log(workbook);
-        console.log(maxCol);
-        console.log(sheetData);
-      };
-      reader.onprogress = (e) => {
-        this.percent = (e.total / e.loaded).toFixed(2);
+        this.sheetNames.push(...workbook.SheetNames);
       };
 
       reader.readAsArrayBuffer(file);
@@ -146,14 +124,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 10px;
-}
-.border-thin {
-  border-width: thin !important;
-}
-.text-ellipsis {
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
 }
 </style>
--- src/components/FileSelect.vue
+++ src/components/FileSelect.vue
@@ -1,39 +1,17 @@
 <template>
-  <b-form-group
-    label-cols="3"
-    label-cols-sm="2"
-    label-size="sm"
-    label="파일 선택"
-    label-for="selectFile"
-    description="참고: xls, xlsx, csv 파일 포맷만 지원합니다."
-  >
-    <b-input-group>
-      <b-form-file
-        id="selectFile"
-        class="col-sm-6 col-lg-5"
-        size="sm"
-        :value="value"
-        :accept="acceptType"
-        :disabled="Boolean(value)"
-        @change="onFileChange($event)"
-      ></b-form-file>
-
-      <!-- input group append slot -->
-      <template v-slot:append>
-        <b-button
-          size="sm"
-          :disabled="!value || loading"
-          @click="$emit('input', null)"
-        >초기화</b-button>
-      </template>
-    </b-input-group>
-
-    <!-- form group description slot -->
-    <template v-slot:description v-if="loading && loadingText">
-      <b-spinner class="border-thin" small></b-spinner>
-      <span class="align-middle ml-1">{{ loadingText }}</span>
-    </template>
-  </b-form-group>
+  <div>
+    <v-file-input
+      dense
+      messages="참고: xls, xlsx, csv 파일 포맷만 지원합니다."
+      :value="value"
+      :accept="acceptType"
+      :clearable="false"
+      :disabled="Boolean(value)"
+      :loading="loading"
+      :placeholder="placeholder"
+      @change="file => $emit('input', file)"
+    ></v-file-input>
+  </div>
 </template>
 
 <script>
@@ -48,21 +26,12 @@ export default {
   props: {
     value: File,
     loading: Boolean,
-    loadingText: String,
+    placeholder: String,
   },
   data() {
     return {
       acceptType: fileAcceptTypes.join(','),
     };
   },
-  methods: {
-    onFileChange(e) {
-      this.$emit('input', e.target.files[0] || null);
-    },
-  },
 };
 </script>
-
-<style>
-
-</style>
